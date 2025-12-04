e4108f7307b6eef5fa2fe28ea848bd87b5e394ef
Jayden Li <jlche18659@gmail.com>
2025-12-03 22:56:13 -0500

    refactor for 2026 libraries

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.commands.SwerveDriveCommand;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.SwerveModule;
+import frc.robot.util.OI;
+
+import java.io.ObjectInputFilter.Config;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  private Drivetrain drivetrain;
+  private OI oi;
+
+  public RobotContainer() {
+    drivetrain = Drivetrain.getInstance();
+    drivetrain.setDefaultCommand(new SwerveDriveCommand());
+
+    oi = OI.getInstance();
+  }
+}
